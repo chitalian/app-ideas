ac21c769a294192680fae7aa4b6b1e1fd5a007b5
Justin <justintorre75@gmail.com>
2022-11-09 20:19:24 -0600

    main ui features and hooking it into backend, also added topics

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,63 +1,91 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { OAICompletion } from '../../lib/OAICompletion'
-import { getEveryPromptFunction, getOpenAICompletion } from '../../lib/util'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { OAICompletion } from "../../lib/OAICompletion";
+import { getEveryPromptFunction, getOpenAICompletion } from "../../lib/util";
 
 const prompt = `You are brainstorming some new awesome AI app ideas following the generative ai hype wave of 2022. What's the best idea for an app to use a combination of LLM's (large language models for text generation), image models (SD/stable diffusion, for image generation) or other modalities.
 
+Topic: Images, Social Network
 Idea: Instagram for SD
 Description: A place where users can generate images for free and share them with their friends
+Topic: Memes, Images
 Idea: Meme generator
 Description: As the name suggests, this will combine LLM + SD to create memes
+Topic: Images, Fun, Playground
 Idea: Whiteboard
 Description: As you are drawing an image it just continuously updates the an AI img2img image at 50% opacity
+Topic: Images, Collaboration, Fun, Playground
 Idea: Whiteboard Multiplayer
 Description: As you are drawing an image it just continuously updates the an AI img2img image at 50% opacity but multiple people are using one canvas
+Topic: Images, Photoshop, Playground
 Idea: SD Photoshop plugin
 Description: Make a plugin where it can take the context of an image and fill in the blanks but within photoshop
+Topic: Technical, Software, Tools
 Idea: Component Library for SD
 Description: Make a good react component library for interacting with SD
+Topic: Game, Fun, Competition
 Idea: Guess the prompt
 Description: Make a global competition to guess the prompt for an image.
+Topic: Business
 Idea: AI Resumes
 Description: Make a resume enhancer
+Topic: Art
 Idea: Tattoo suggestions
 Description: Give you tattoo suggestions using SD
+Topic: Tools, Playground
 Idea: Image DAG picker
 Description: Given a prompt you are now given 4 images, and then you can keep doing img2img until you find the img you want, but visualized as a dag
+Topic: Software, Psychology
 Idea: Sentiment analysis
 Description: Given a body of text, generate a sentiment analysis
+Topic: Software, Engineering
 Idea: zsh autocompletion pilot
 Description: As you are typing in your terminal can you get suggestions of commands to run
+Topic: Education
 Idea: text highlight an explain
 Description: Select any text and explain it
+Topic: Software, Developer
 Idea: FauxPilot VSCode extension
-Description: Build a good extension for Faux Pilot, an open-source alternative to GitHub CoPilot
-Idea: SD Generator + Up Scaler
-Description: Build a site where you can use SD and then pipe the result into an AI upscaler
-Idea:`
+Description: Build a good extension for Faux Pilot, an open-source alternative to GitHub CoPilot`;
 
 interface Error {
-  error: string
+  error: string;
 }
 
 interface Result {
-  idea: string
-  description: string
+  idea: string;
+  description: string;
+}
+
+function parseResults(
+  completion: string
+): [string, string, string] | undefined {
+  const topicSplit = completion.split("Topic: ");
+  if (topicSplit.length != 2) {
+    return undefined;
+  }
+  const topic = topicSplit[0];
+
+  const descriptionSplit = completion.split("Description: ");
+  if (descriptionSplit.length != 2) {
+    return undefined;
+  }
+  const idea = descriptionSplit[0].replace("Idea:", "");
+  const description = descriptionSplit[1];
+  return [topic, idea, description];
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Error | Result>
 ) {
-  if(req.method !== 'POST') {
-    res.status(405).json({ error: 'Method not allowed' })
-    return
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method not allowed" });
+    return;
   }
 
-  let { keywords, model } = req.body
+  let { keywords, model } = req.body;
 
-  const variables = [keywords]
   /*
   let epFunction = await getEveryPromptFunction('app-idea-generator-0oTaY4', 'ai-app-ideas')
   if(epFunction) {
@@ -85,21 +113,27 @@ export default async function handler(
     res.status(404).json({ error: 'Not found.' })
   }
   */
+  console.log("KEYBOARD", keywords);
+
   let completion = await getOpenAICompletion(
-    prompt,
+    keywords === ""
+      ? `${prompt}\nTopic:`
+      : `${prompt}\nTopic:${keywords}\nIdea:`,
     model,
     100,
     1.0,
     1.0,
     1.0,
-    ["Idea: "],
-  )
+    ["Topic: "]
+  );
 
-  if(completion) {
-    let idea = completion.split('Description: ')[0].replace('Idea: ', '')
-    let description = completion.split('Description: ')[1].split('Idea: ')[0]
-    res.status(200).json({ idea, description })
+  if (completion) {
+    console.log(parseResults(completion));
+
+    let idea = completion.split("Description: ")[0].replace("Idea: ", "");
+    let description = completion.split("Description: ")[1].split("Idea: ")[0];
+    res.status(200).json({ idea, description });
   } else {
-    res.status(404).json({ error: 'Not found.' })
+    res.status(404).json({ error: "Not found." });
   }
 }
