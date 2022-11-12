d2db8acfebec239721250394ea1df33f27605684
Justin <justintorre75@gmail.com>
2022-11-12 02:22:26 -0600

    added discord icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,15 @@ import { ImGithub } from "react-icons/im";
 import { HeartIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import { v4 as uuidv4 } from "uuid";
 
+function DiscordIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg aria-hidden="true" viewBox="4 2 18 22" {...props}>
+      <path d="M9.593 10.971c-.542 0-.969.475-.969 1.055 0 .578.437 1.055.969 1.055.541 0 .968-.477.968-1.055.011-.581-.427-1.055-.968-1.055zm3.468 0c-.542 0-.969.475-.969 1.055 0 .578.437 1.055.969 1.055.541 0 .968-.477.968-1.055-.001-.581-.427-1.055-.968-1.055z" />
+      <path d="M17.678 3H4.947A1.952 1.952 0 0 0 3 4.957v12.844c0 1.083.874 1.957 1.947 1.957H15.72l-.505-1.759 1.217 1.131 1.149 1.064L19.625 22V4.957A1.952 1.952 0 0 0 17.678 3zM14.01 15.407s-.342-.408-.626-.771c1.244-.352 1.719-1.13 1.719-1.13-.39.256-.76.438-1.093.562a6.679 6.679 0 0 1-3.838.398 7.944 7.944 0 0 1-1.396-.41 5.402 5.402 0 0 1-.693-.321c-.029-.021-.057-.029-.085-.048a.117.117 0 0 1-.039-.03c-.171-.094-.266-.16-.266-.16s.456.76 1.663 1.121c-.285.36-.637.789-.637.789-2.099-.067-2.896-1.444-2.896-1.444 0-3.059 1.368-5.538 1.368-5.538 1.368-1.027 2.669-.998 2.669-.998l.095.114c-1.71.495-2.499 1.245-2.499 1.245s.21-.114.561-.275c1.016-.446 1.823-.57 2.156-.599.057-.009.105-.019.162-.019a7.756 7.756 0 0 1 4.778.893s-.751-.712-2.366-1.206l.133-.152s1.302-.029 2.669.998c0 0 1.368 2.479 1.368 5.538 0-.001-.807 1.376-2.907 1.443z" />
+    </svg>
+  );
+}
+
 interface Idea {
   description: string;
   favorite: boolean;
@@ -86,8 +95,11 @@ export default function Home() {
         </Head>
         <div className="flex flex-col w-full">
           <h1 className="flex flex-row justify-end p-2 gap-2">
+            <a href="https://discord.gg/Xn3kYXkFN8">
+              <DiscordIcon className="w-7 h-7" />
+            </a>
             <a href="https://github.com/chitalian/app-ideas">
-              <ImGithub className="w-5 h-5" />
+              <ImGithub className="w-7 h-7" />
             </a>
           </h1>
           <h1 className="text-center text-4xl p-8 w-full">AI App Ideas</h1>
